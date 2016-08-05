97947032fa8514ccc654bf710428517e7d87c2dd
Alex Flores <Alex Flores>
2016-08-04 22:03:13 -0400

    Se crea aplicacion basica de servidor web

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const http = require('http')
+const port = process.env.PORT || 8080
+const server = http.createServer(function(req,res){
+  res.end('Hola mundo! io.js')
+})
+
+server.listen(port, function(){
+  console.log('Servidor escuchando en puerto '+ port);
+})
