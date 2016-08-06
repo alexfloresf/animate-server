a9581d9b2f1f35fedb7d227d0915b2e822a6160c
alexfloresf <alex.flores.fuentealba@gmail.com>
2016-08-06 16:19:55 -0400

    Se ha modificado el server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const http = require('http')
+const port = process.env.PORT || 8080
+const server = http.createServer()
+
+server.on('request', onRequest)
+server.on('listening', onListening)
+
+server.listen(port)
+
+function onRequest(req,res){
+  res.end('Hola mundo! io.js')
+}
+
+function onListening(){
+  console.log('Servidor escuchando en puerto '+ port)
+}
