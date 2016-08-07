afa2cb75bb803bcbcc636bb128e283dbcac57a0f
alexfloresf <alex.flores.fuentealba@gmail.com>
2016-08-07 15:37:08 -0400

    Se modifico el server.js para manejar Callbacks, Rutas y Archivos asíncronos

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const http = require('http')
 const fs = require('fs')
+const path = require('path')
 const port = process.env.PORT || 8080
 const server = http.createServer()
 
@@ -11,9 +12,14 @@ server.on('listening', onListening)
 server.listen(port)
 
 function onRequest(req,res){
+  let fileName = path.join(__dirname,'public','index.html')
 //  res.end('Hola mundo! io.js')
-  let file = fs.readFileSync('public/index.html')
+  fs.readFile(fileName, function(err,file){
+    if (err) {
+      return res.end(err.message)
+    }
   res.end(file)
+  })
 }
 
 function onListening(){
