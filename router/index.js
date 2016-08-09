79ef401cc22996ef854b107b840f71ca998da635
alexfloresf <alex.flores.fuentealba@gmail.com>
2016-08-09 00:25:27 -0400

    Se crea apricacion de soporte en Ajax

--- router/index.js
+++ router/index.js
@@ -1,12 +1,25 @@
 const path = require('path')
 const course = require('course')
 const st = require('st')
+const jsonBody = require('body/json')
 
 const router= course()
 const mount = st({
   path: path.join(__dirname, '..', 'public'),
   index: 'index.html',
   passthrough: true
+})
+
+router.post('/process', function(req,res){
+  jsonBody(req,res,{
+    limit: 3 * 1024 * 1024
+  },function(err,body){
+    if(err) return fail(err,res)
+    console.log(body)
+
+    res.setHeader('Content-Type','application/json')
+    res.end(JSON.stringify({ok: true}))
+  })
 })
 
 function onRequest (req, res) {
@@ -16,7 +29,7 @@ function onRequest (req, res) {
   router(req,res, function(err){
     if (err) return fail(err, res)
     res.statusCode = 404
-    res.end(`404 Not Found: ${req.url}`)    
+    res.end(`404 Not Found: ${req.url}`)
     } )
   })
 }
